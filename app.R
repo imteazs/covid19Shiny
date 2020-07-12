93f35261d978488cc5740ed96c2dda13498080bc
sidnite <imteazs@gmail.com>
2020-07-11 22:27:49 -0700

    adding county map

--- app.R
+++ app.R
@@ -10,20 +10,31 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(leaflet)
+library(maps)
 
 stateData = data.frame(read.csv('statewide_cases.csv'))
 stateData$dateConvert = as.Date(stateData$date, '%Y-%m-%d')
+counties = sort(unique(stateData$county))
 # Define UI for application that draws a histogram
 ui <- navbarPage(
     'California COVID19 Visualizer',
-    tabPanel('CA Infection Rate'),
+    tabPanel('CA Infection Rate',
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(label = 'Test1', choices = c('blah', 'bhal'), inputId = 'grr')
+               ),
+               mainPanel(
+                 leafletOutput('mycamap')
+               )
+             )),
     tabPanel('County Status',
              sidebarLayout(
                  sidebarPanel(
                      selectInput(
                          label = 'Select County',
                          inputId = 'countySelect',
-                         choices = unique(stateData$county)
+                         choices = counties
                          ),
                      selectInput(
                          label = 'Select Variables',
@@ -38,27 +49,34 @@ ui <- navbarPage(
                      plotOutput('outfig')
                      )
                  )
-             )
+             ),
+    tabPanel('About')
     )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  output$mycamap = renderLeaflet({
+    mapCounties = map("county", "california", fill = TRUE, plot = FALSE)
+    leaflet(data = mapCounties) %>% addTiles() %>%
+      addPolygons(fillColor = topo.colors(30, alpha = NULL), stroke = FALSE)
+  })
+  
   output$outfig = renderPlot({
     if(input$colSelect == 'Total Positive Cases'){
       newData = filter(stateData, county == input$countySelect)
-      ggplot(data = newData, aes(dateConvert, totalcountconfirmed)) + geom_line()
+      ggplot(data = newData, aes(dateConvert, totalcountconfirmed)) + geom_line() + labs(x = 'Dates', y = 'Total Cases')
     }
     else if(input$colSelect == 'Total Deaths'){
       newData = filter(stateData, county == input$countySelect)
-      ggplot(data = newData, aes(dateConvert, totalcountdeaths)) + geom_line()
+      ggplot(data = newData, aes(dateConvert, totalcountdeaths)) + geom_line() + labs(x = 'Dates', y = 'Total Deaths')
     }
     else if(input$colSelect == 'New Cases'){
       newData = filter(stateData, county == input$countySelect)
-      ggplot(data = newData, aes(dateConvert, newcountconfirmed)) + geom_line()
+      ggplot(data = newData, aes(dateConvert, newcountconfirmed)) + geom_line() + labs(x = 'Dates', y = 'New Cases')
     }
     else if(input$colSelect == 'New Deaths'){
       newData = filter(stateData, county == input$countySelect)
-      ggplot(data = newData, aes(dateConvert, newcountdeaths)) + geom_line()
+      ggplot(data = newData, aes(dateConvert, newcountdeaths)) + geom_line() + labs(x = 'Dates', y = 'New Deaths')
     }
   })
 }
